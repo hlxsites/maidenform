e7e329a65f02289c28ad384a6e4b86a1d6bbda20
Mark J. Becker <herzog31@users.noreply.github.com>
2022-12-12 14:05:17 +0100

    Update naming of column styles

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -2,8 +2,8 @@ export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
-  // Handling of variant 4
-  if (block.classList.contains('variant4')) {
+  // Handling of teaser style
+  if (block.classList.contains('teaser')) {
     if (cols.length > 1) {
       // Take second column div and move into first column
       cols[0].appendChild(cols[1]);
