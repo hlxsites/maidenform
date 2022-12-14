ef112dcea8154dd7c02f204aa3f5722a886366b4
Mark J. Becker <herzog31@users.noreply.github.com>
2022-12-14 10:53:35 +0100

    Refactor hover-cards style into own block

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,5 +1,3 @@
-import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
-
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
@@ -10,24 +8,5 @@ export default function decorate(block) {
       // Take second column div and move into first column
       cols[0].appendChild(cols[1]);
     }
-  }
-
-  // Handling of hover-cards style
-  if (block.classList.contains('hover-cards')) {
-    cols.forEach((col) => {
-      // Move picture and content into separate divs
-      const imgContainer = document.createElement('div');
-      const contentContainer = document.createElement('div');
-      imgContainer.appendChild(col.querySelector('picture'));
-      imgContainer.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '500' }])));
-      const h3 = col.querySelector('h3');
-      contentContainer.append(...col.children);
-      contentContainer.querySelectorAll('p').forEach((p) => {
-        if (p.innerHTML.trim() === '') {
-          p.remove();
-        }
-      });
-      col.append(imgContainer, contentContainer, h3);
-    });
   }
 }
