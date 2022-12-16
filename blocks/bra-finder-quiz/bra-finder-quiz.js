d09b1b26305863b76b86641296a7cef4474ac1c2
Hannes Hertach <hanneshertach@gmail.com>
2022-12-16 16:49:42 +0100

    implement bra finder logic

---
+++ blocks/bra-finder-quiz/bra-finder-quiz.js
@@ -0,0 +1,83 @@
+import { toClassName } from '../../scripts/lib-franklin.js';
+
+function deselectAll(parent) {
+  parent.querySelectorAll('[aria-selected="true"]').forEach((selected) => {
+    selected.removeAttribute('aria-selected');
+  });
+}
+
+function getCurrentQuestion(block) {
+  return block.querySelector('.question:not([hidden="true"])');
+}
+
+function checkCanAdvance(block) {
+  const currentQuestion = getCurrentQuestion(block);
+
+  const canAdvance = [...currentQuestion.querySelectorAll('ul')]
+    .reduce((acc, questionPart) => acc && questionPart.querySelector('[aria-selected="true"]'), true);
+
+  if (canAdvance) {
+    block.querySelector('button').removeAttribute('disabled');
+  }
+}
+
+function convertToValue(li) {
+  const strong = li.querySelector(':scope > strong');
+  if (strong && strong.children.length === 0) {
+    return strong.innerText.split(' ').join('');
+  }
+
+  return toClassName(li.innerText.split(' ').join(''));
+}
+
+function collectSelections(block) {
+  const searchParams = new URLSearchParams();
+
+  block.querySelectorAll('ul').forEach((ul) => {
+    // const value
+    const selectedLi = ul.querySelector('li[aria-selected="true"]');
+    const value = convertToValue(selectedLi);
+    searchParams.append(ul.getAttribute('field-name'), value);
+  });
+  return searchParams;
+}
+
+export default function decorate(block) {
+  ['title', 'question q1', 'question q2', 'question q3', 'question q4'].forEach((name, i) => {
+    block.children[i]?.classList.add(...name.split(' '));
+  });
+
+  block.querySelectorAll('.question').forEach((question, i) => {
+    if (i !== 0) question.setAttribute('hidden', 'true');
+  });
+
+  block.querySelectorAll('ul > li:first-child > em').forEach((fieldNameContainer, i) => {
+    const fieldName = fieldNameContainer?.innerText ?? `field-${i}`;
+    fieldNameContainer.closest('ul').setAttribute('field-name', fieldName);
+    fieldNameContainer.closest('li').remove();
+  });
+
+  const nextButton = document.createElement('button');
+  nextButton.innerText = 'Next';
+  nextButton.setAttribute('disabled', 'true');
+  nextButton.addEventListener('click', () => {
+    nextButton.setAttribute('disabled', 'true');
+    const currentQuestion = getCurrentQuestion(block);
+    currentQuestion.setAttribute('hidden', 'true');
+    const nextQuestion = currentQuestion.nextElementSibling;
+    if (nextQuestion && nextQuestion.classList.contains('question')) {
+      nextQuestion.removeAttribute('hidden');
+    } else {
+      window.location = `/bra-fit-finder/result?${collectSelections(block).toString()}`;
+    }
+  });
+  block.append(nextButton);
+
+  block.querySelectorAll('.question > div > ul > li').forEach((questionChoice) => {
+    questionChoice.addEventListener('click', () => {
+      deselectAll(questionChoice.closest('ul'));
+      questionChoice.setAttribute('aria-selected', 'true');
+      checkCanAdvance(block);
+    });
+  });
+}
--- blocks/bra-finder-results/bra-finder-results.js
+++ blocks/bra-finder-results/bra-finder-results.js
@@ -1,32 +1,95 @@
+function substituteTemplates(block, templates) {
+  [...block.querySelectorAll('em')].forEach((el) => {
+    const result = el.innerText.match(/{{(\w+)}}/);
+    const templateKey = result?.[1];
+    if (templateKey && templates[templateKey]) {
+      el.innerText = el.innerText.replace(`{{${templateKey}}}`, templates[templateKey]);
+      el.classList.add('substitute-template');
+      el.parentElement.classList.add(`substitute-${templateKey}`);
+    }
+  });
+}
+
+async function decorateSimpleResults(block, bandSize, cupSize) {
+  const braSizesResponse = await fetch('/brasizes.json');
+  const braSizes = await braSizesResponse.json();
+
+  const recommendation = braSizes.sizes?.data?.find(
+    ({
+      band,
+      cup
+    }) => bandSize === band && cupSize === cup,
+  );
+
+  if (!recommendation) {
+    block.innerHTML = '<a href="/bra-finder">An error occurred. Click to go back.</a>';
+    return;
+  }
+
+  const templateSubstitutes = {
+    size: recommendation.size,
+    alt1: recommendation.alternative1,
+    alt2: recommendation.alternative2,
+  };
+
+  substituteTemplates(block, templateSubstitutes);
+}
+
+async function decorateExtendedResults(block, bandSize, cupSize, bandFit, cupFit, silhouette) {
+  const braSizesResponse = await fetch('/brasizes.json');
+  const braSizes = await braSizesResponse.json();
+
+  console.log(braSizes['finder-32']?.data);
+
+  const recommendation = braSizes['finder-32']?.data?.find(
+    (rec) => bandSize === rec.band
+      && cupSize.toLowerCase() === rec.cup.toLowerCase()
+      && bandFit === rec.bandFit
+      && rec.cupsFit === cupFit && rec.silhouette === silhouette,
+  );
+
+  if (!recommendation) {
+    block.innerHTML = '<a href="/bra-finder">An error occurred. No recommendation found. Click to go back.</a>';
+    return;
+  }
+
+  console.log(recommendation)
+
+  const templateSubstitutes = {
+    size: recommendation.size,
+    alt1: recommendation.alternative1,
+    alt2: recommendation.alternative2,
+    recommendation: recommendation.recommendation,
+  };
+
+  substituteTemplates(block, templateSubstitutes);
+
+  const otherSilhouettes = [...block.querySelectorAll('ul')].find((ul) => ul.querySelector('li:first-child > em')?.innerText === '{{other-sihlouettes}}');
+  if (otherSilhouettes) {
+    otherSilhouettes.children[0].remove();
+    console.log([...otherSilhouettes.children]
+      .map((child) => child.innerText.trim().toLowerCase().replaceAll(/[- ]/g, '')));
+    [...otherSilhouettes.children]
+      .find((child) => child.innerText.trim().toLowerCase().replaceAll(/[- ]/g, '') === silhouette)?.remove();
+    while (otherSilhouettes.children.length > 3) {
+      otherSilhouettes.children[0].remove();
+    }
+  }
+}
+
 export default async function decorate(block) {
   const params = new URLSearchParams(window.location.search);
 
-  if (params.get('band-size') && params.get('cup-size')) {
-    const braSizesResponse = await fetch('/brasizes.json');
-    const braSizes = await braSizesResponse.json();
+  const bandSize = params.get('band-size');
+  const cupSize = params.get('cup-size');
+  const bandFit = params.get('band-fit');
+  const cupFit = params.get('cup-fit');
+  const silhouette = params.get('silhouette');
 
-    const recommendation = braSizes.sizes?.data?.find(({ bandSize, cupSize }) => bandSize === params.get('band-size') && cupSize === params.get('cup-size'));
-
-    if (!recommendation) {
-      block.innerHTML = '<a href="/bra-finder">An error occurred. Click to go back.</a>';
-      return;
-    }
-
-    const templateSubstitutes = {
-      size: recommendation.size,
-      alt1: recommendation.alternative1,
-      alt2: recommendation.alternative2,
-    };
-
-    [...block.querySelectorAll('em')].forEach((el) => {
-      const result = el.innerText.match(/{{(\w+)}}/);
-      const templateKey = result?.[1];
-      if (templateKey && templateSubstitutes[templateKey]) {
-        el.innerText = el.innerText.replace(`{{${templateKey}}}`, templateSubstitutes[templateKey]);
-        el.classList.add('substitute-template');
-        el.parentElement.classList.add(`substitute-${templateKey}`);
-      }
-    });
+  if (bandSize && cupSize && bandFit && cupFit && silhouette) {
+    await decorateExtendedResults(block, bandSize, cupSize, bandFit, cupFit, silhouette);
+  } else if (bandSize && cupSize) {
+    await decorateSimpleResults(block, bandSize, cupSize);
   } else {
     block.innerHTML = '<a href="/bra-finder">An error occurred. Click to go back.</a>';
   }
